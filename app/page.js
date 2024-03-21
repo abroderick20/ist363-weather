b312fa5391e1a669f82501d79f1f1b9778e6e937
abroderick20 <albroder@syr.edu>
2024-03-21 18:20:35 -0400

    attendance 03/21/24: Components, props and SASS styling

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
+import Button from "../components/Button";
+
 const Homepage = () => {
-  return <div>Whooo</div>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <Button label="download" />
+      <Button label="register now" />
+      <Button label="learn more" />
+    </div>
+  );
 };
 export default Homepage;
